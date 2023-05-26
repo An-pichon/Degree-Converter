1b2ea85e2931733dcbe47fd117e5ad175e2f327f
MrR4P70R <R4P70RX@gmail.com>
2023-05-26 16:15:00 +0200

    Adding color to string version of Temperature class

--- package/temperature/Temperature.py
+++ package/temperature/Temperature.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Union
 
+from colorama import Fore, Style
+
 from package.temperature.units.TemperatureUnit import TemperatureUnit
 
 
@@ -21,5 +23,15 @@ class Temperature(ABC):
     def to(self, unit: TemperatureUnit):
         return self.__getattribute__(f"to_{unit.value}")()
 
+    @property
+    def _temperature(self):
+        # Temperature style
+        return f"{Style.BRIGHT}{Fore.CYAN}{self.temperature}°{Fore.RESET}{Style.RESET_ALL}"
+
+    @property
+    def _unit(self):
+        # Temperature style
+        return f"{Style.BRIGHT}{Fore.MAGENTA}{self.unit.value}{Fore.RESET}{Style.RESET_ALL}"
+
     def to_string(self):
-        return f"{self.temperature}° {self.unit.value}"
+        return f"{self._temperature} {self._unit}"
